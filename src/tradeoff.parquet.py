127746b8b7685bd507b217987a9eb57b7d66bde2
Jonathan St-Onge <jonathanstonge7@gmail.com>
2025-03-09 22:12:48 -0400

    last try

--- src/tradeoff.parquet.py
+++ src/tradeoff.parquet.py
@@ -4,24 +4,30 @@ import pyarrow as pa
 import pyarrow.parquet as pq
 import sys
 
-DAT_DIR = Path("src/")
+# DAT_DIR = Path("src/")
 
-dfs = []
-for mydir in DAT_DIR.glob("*_data_dir"):
-    for csvfile in mydir.glob("test_*"):
+# dfs = []
+# for mydir in DAT_DIR.glob("*_data_dir"):
+#     for csvfile in mydir.glob("test_*"):
         
-        df=pd.read_csv(csvfile, names=["time", "d1", "d2", "y", "costDeathsCum", "avgProgs"])
-        df['beta']=csvfile.stem.split('_')[1]
-        df['k']=mydir.stem.split('_')[0].split('k')[1]
+#         df=pd.read_csv(csvfile, names=["time", "d1", "d2", "y", "costDeathsCum", "avgProgs"])
+#         df['beta']=csvfile.stem.split('_')[1]
+#         df['k']=mydir.stem.split('_')[0].split('k')[1]
 
-        dfs.append(df)
+#         dfs.append(df)
 
-df = pd.concat(dfs)
+# df = pd.concat(dfs)
+
+
 
 # To reduce memory usage
-scale_factor = 1000  # Convert to a large integer scale
-df['time_scaled'] = (df['time'] * scale_factor).astype(int)
-df = df[df['time_scaled'] % int(0.03 * scale_factor) == 0].drop(columns=['time_scaled']).reset_index(drop=True)
+# scale_factor = 1000  # Convert to a large integer scale
+# df['time_scaled'] = (df['time'] * scale_factor).astype(int)
+# df = df[df['time_scaled'] % int(0.03 * scale_factor) == 0].drop(columns=['time_scaled']).reset_index(drop=True)
+
+# df.to_csv("tradeoff.csv", index=False)
+
+df = pd.read_csv("src/tradeoff.csv")
 
 buf = pa.BufferOutputStream()
 table = pa.Table.from_pandas(df)
