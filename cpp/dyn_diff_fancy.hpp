70e947b3eeecd8490db5c61cc16d45417ab92028
Jonathan St-Onge <jonathanstonge7@gmail.com>
2025-03-20 09:03:46 -0400

    new fancy dyn

---
+++ cpp/dyn_diff_fancy.hpp
@@ -0,0 +1,164 @@
+#ifndef DYN_DIFF_HPP_INCLUDED
+#define DYN_DIFF_HPP_INCLUDED
+
+#include <boost/multi_array.hpp>
+#include <boost/math/distributions/poisson.hpp>
+#include <math.h>
+
+using namespace std;
+
+/**
+ * @file   dyn_diff.hpp
+ * @brief  ODE system for research group dynamics in a master equation system
+ *
+ * @author  LHD & JSO
+ * @since   2025-03-04
+ */
+
+
+struct Sparam {
+    const double mu;
+    const double nu_n;
+    const double nu_p;
+    const double ktau;
+    const double a;
+    const double bl;
+    const double kc;
+    const double x0c;
+    const double K;
+    const int max1;
+    const int max2;
+}; // parameter structure
+
+double cost(double x, double bl, double kc, double x0c, double r) {
+
+    double value = 0.0;    
+    value = (1 - r*bl)*(1. / (1. + exp(kc * (x - x0c)))) + bl;
+    
+    if(value<1.0 && value>0) return value;
+    else return 0.0;
+}
+
+double B(double x, double bl, double a, double r) {
+    return (1. - r*bl)*exp(-a*x) + bl;   
+}
+
+double PI(double x, double a, double bl, double r) {
+    return (1. - r*bl)*exp(-a*x) + bl;
+}
+
+double tau(double x, double ktau, double a, double bl, double kc, double x0c, double r) {
+    double value = 0.0;
+    value = 1.0 / (1.0 + exp(-ktau * (B(x, bl, a, r) - cost(x, bl, kc, x0c, r) + PI(x, a, bl, r))));
+    if(value<1.0 && value>0.0) return value;
+    else return 0.0;
+}
+
+double sigma(int n, int p, double k, double m) {
+    double value = m*(n+p+1) * (1.0-(1.0+n+p)/(1.0*k));
+    if(value>0) return value;
+    else return 0.0;
+}
+
+
+//********** function dydt definition **************************************************************
+int dydt(double t, const double y[], double f[], void * param) {
+    Sparam& p = *static_cast<Sparam* >(param);
+
+    // Create multi_array reference to y and f
+    typedef boost::multi_array_ref<const double,2> CSTmatref_type;
+    typedef boost::multi_array_ref<double,2> matref_type;
+    
+    typedef CSTmatref_type::index indexref;
+
+    // The distribution is in the first (p.max1 * p.max2) entries
+    // The final entry is Z.
+    CSTmatref_type yref(y, boost::extents[p.max1][p.max2]);
+    matref_type    fref(f, boost::extents[p.max1][p.max2]);
+
+    // hidden parameter
+    double r = 5.0;
+
+    // Process all dynamics
+    double R_costDeath = 0.0;
+    for(int d1=0; d1<p.max1; ++d1) {  //loop over number of non-programmers
+      for(int d2=0; d2<p.max2; ++d2) { //loop over number of programmers
+
+            fref[d1][d2] = 0;
+            const double gsize = d1 + d2;
+
+            if(d1+d2<=p.K) {
+
+                
+                // birth output
+                if(d1 < p.max1 - 1) {
+                    fref[d1][d2] -= sigma(d1, d2, p.K, p.mu) * yref[d1][d2];
+                } // birth input
+                if(d1 > 0) {
+                    fref[d1][d2] += sigma(d1 - 1, d2, p.K, p.mu) * yref[d1 - 1][d2];
+                }
+                
+                // non-prog deaths output
+                if(d1 < p.max1 - 1) {
+                    fref[d1][d2] += p.nu_n * (d1 + 1) * yref[d1 + 1][d2];
+                }
+                fref[d1][d2] -= p.nu_n * d1 * yref[d1][d2];
+
+                // prog deaths output
+                if(d2 < p.max2 - 1) {
+                    fref[d1][d2] += p.nu_p * (d2 + 1) * yref[d1][d2 + 1];
+                }
+                fref[d1][d2] -= p.nu_p * d2 * yref[d1][d2];
+
+                // non-prog -> prog transitions output
+                if (gsize > 0.0) {
+
+                    R_costDeath += tau(d2/gsize, p.ktau, p.a, p.bl, p.kc, p.x0c, r)
+                                        * d1
+                                        * cost(d2/(d1+d2), p.bl, p.kc, p.x0c, r)
+                                        * yref[d1][d2];
+
+                    if (d2 < p.max2-1) {
+                        // Predation input
+                        fref[d1][d2] -= tau(d2/gsize, p.ktau, p.a, p.bl, p.kc, p.x0c, r)
+                            * (1.0 - cost(d2/gsize, p.bl, p.kc, p.x0c, r))
+                            * d1
+                            * yref[d1][d2];
+                    }
+                    
+                                            
+                    if(d1 < p.max1-1 && d2 > 0 && d2 < p.max2-1) {
+                        // Predation output
+                        fref[d1][d2] += tau((d2-1.0)/gsize, p.ktau, p.a, p.bl, p.kc, p.x0c, r)
+                                        * (d1 + 1.0)
+                                        * (1.0 - cost((d2-1.0)/gsize, p.bl, p.kc, p.x0c, r))
+                                        * yref[d1 + 1][d2 - 1];
+                    }
+                    
+                    
+                    fref[d1][d2] -= tau(d2/gsize, p.ktau, p.a, p.bl, p.kc, p.x0c, r)
+                                    * d1
+                                    * cost(d2/gsize, p.bl, p.kc, p.x0c, r)
+                                    * yref[d1][d2]; 
+                }
+
+                // Predation output
+                if(d1 < p.max1 - 1) {
+                    fref[d1][d2] += tau((d2)/(gsize+1.0), p.ktau, p.a, p.bl, p.kc, p.x0c, r)
+                                    * (d1 + 1.0)
+                                    * cost((d2)/(gsize+1.0), p.bl, p.kc, p.x0c, r)
+                                    * yref[d1 + 1][d2];
+                }
+                
+                
+            }
+            if (d1==p.max1-1.0 && d2==p.max2-1.0) fref[d1][d2] = R_costDeath;
+      }
+    }
+
+
+    return GSL_SUCCESS;
+
+} //********** end function dydt definition ********************************************************
+
+#endif // DYN_DIFF_HPP_INCLUDED
---
+++ cpp/tevol_source_diff_fancy.cpp
@@ -0,0 +1,169 @@
+/**
+ * @file   tevol_source_diff.hpp
+ * @brief  ODE system for research group dynamics in a master equation system
+ *
+ * Compile with:
+ * g++ -std=c++11 -O3 -o tevol_source_diff ./tevol_source_diff.cpp $(gsl-config --cflags) $(gsl-config --libs)
+ *
+ * @author  LHD + JSO
+ * @since   2023-11-28
+ */
+
+ #include <iostream>
+ #include <cmath>
+ #include <algorithm>
+ #include <vector>
+ #include <fstream>
+ #include <sstream>
+ 
+ #include <boost/multi_array.hpp>
+ #include <gsl/gsl_errno.h>
+ #include <gsl/gsl_odeiv.h>
+ 
+ #include "dyn_diff_fancy.hpp"
+ 
+ using namespace std;
+ 
+ int main(int argc, const char *argv[]) {
+     
+     if(argc < 13) {
+         cerr << "Usage: ./tevol_source_diff mu nu_n nu_p alpha beta k x0 K max1 max2 IC TEMPORAL [t_max]\n";
+         return 1;
+     }
+     // Model parameters
+     double mu = stod(argv[1]);
+     double nu_n = stod(argv[2]);
+     double nu_p = stod(argv[3]);
+     double ktau = stod(argv[4]);
+     double a = stod(argv[5]);
+     double bl = stod(argv[6]);
+     double kc = stod(argv[7]);
+     double x0c = stod(argv[8]);
+     double K = stod(argv[9]);
+     int max1 = stoi(argv[10]);
+     int max2 = stoi(argv[11]);
+     int IC = stoi(argv[12]);
+     int TEMPORAL = atoi(argv[13]);
+
+     double t_max = (argc > 14) ? stod(argv[14]) : 10.0;
+
+     Sparam param = {mu, nu_n, nu_p, ktau, a, bl, kc, x0c, K, max1, max2};
+ 
+     // Integrator parameters
+     double t = 0.0;
+     double dt = 0.1;
+     double t_step = 0.01;
+    //  double t_step = 0.01;
+    //  double t_step = 0.5;
+     const double eps_abs = 1e-6;
+     const double eps_rel = 1e-8;
+ 
+     // State space (probability matrix)
+     typedef boost::multi_array<double, 2> mat_type;
+     mat_type y(boost::extents[max1][max2]);
+     fill(y.data(), y.data() + y.num_elements(), 0.0);
+ 
+     // Initial condition
+     double last_avg = 0.0;
+     if (IC == 0) {
+         y[0][0] = 1.0; // all non-programmers
+         last_avg = 0.0;
+     } else if (IC == 1) {
+         y[0][max2 - 1] = 1.0; // all programmers
+         last_avg = 1.0;
+     } else if (IC == 3) {
+         y[max1 - 1][0] = 1.0; // large group, all non-programmers
+         last_avg = 0.0;
+     } else if (IC == 4) {
+        y[35][0] = 1.0; // some ratio
+        last_avg = 0.0;
+    } else {
+         cerr << "Invalid IC value.\n";
+         return 1;
+     }
+
+ 
+     // GSL setup
+     const gsl_odeiv_step_type *step_type = gsl_odeiv_step_rkf45;
+     gsl_odeiv_step *step = gsl_odeiv_step_alloc(step_type, max1 * max2);
+     gsl_odeiv_control *control = gsl_odeiv_control_y_new(eps_abs, eps_rel);
+     gsl_odeiv_evolve *evolve = gsl_odeiv_evolve_alloc(max1 * max2);
+     gsl_odeiv_system sys = {dydt, NULL, static_cast<size_t>(max1 * max2), &param};
+
+     // What we'll track 
+     double last0 = 0.0;
+     double diff = 1.0;
+
+     double costDeathsCum = 0.0;  // cumulative cost-based deaths
+     double t_prev = 0.0;         // track previous time for dt
+
+       // cout << t << ", mu:" << mu << ", nu_n: " << nu_n << ",nu_p: " << nu_p << ", ktau: "
+        // << ktau << ", a: " << a << ", bl: " << bl << ", x0c: " << x0c << ", K: "
+        // << K << ", IC:" << IC << ", AVG PROGS:"  << avg_progs << ", CUM DEATHS: "  << y[max1-1][max2-1]  <<
+        // ", TEMPORAL: " << TEMPORAL  << "\n";
+
+    // Main loop
+    for (double t_target = t + t_step; t < t_max; t_target += t_step) {
+    // for (double t_target = t + t_step; diff > 1e-6; t_target += t_step) {
+        while (t < t_target) {
+            int status = gsl_odeiv_evolve_apply(evolve, control, step, &sys, &t, t_target, &dt, y.data());
+            if (status != GSL_SUCCESS) {
+                cerr << "GSL integration failed.\n";
+                return 1;
+            }
+        }
+
+        double avg_progs = 0.0;
+        for (int d1 = 0; d1 < max1; ++d1) {
+            for (int d2 = 0; d2 < max2; ++d2) {
+                if(d1+d2>K) continue;
+                avg_progs += d2*y[d1][d2];
+            }
+        }
+        
+
+        diff = fabs(last0 - avg_progs);
+        last0 = avg_progs;
+
+        if (TEMPORAL == 1) {
+            // Dump full matrix plus the cumulative cost-based deaths
+            for (int d1 = 0; d1 < max1; ++d1) {
+                for (int d2 = 0; d2 < max2; ++d2) {
+                    if(d1+d2<=K) {
+                        cout << t << "," << d1 << "," << d2 << ","
+                            << y[d1][d2] << "," 
+                            << y[max1-1][max2-1] << "," << avg_progs << "\n";
+                    }
+                    else cout << t << "," << d1 << "," << d2 << ","
+                            << 0.0 << "," 
+                            << y[max1-1][max2-1] << "," << avg_progs << "\n";
+                }
+            }
+        }
+        
+
+        // For equilibrium mode, print final aggregated line
+        if (TEMPORAL == 0) {
+            double avg = 0.0;
+            for (int d1 = 0; d1 < max1; ++d1) {
+                for (int d2 = 0; d2 < max2; ++d2) {
+                    if (d1 + d2 > 0) {
+                        avg += (1.0 * d2)/(d1 + d2) * y[d1][d2];
+                    }
+                }
+            }
+            cout << t << "," << mu << "," << nu_n << "," << nu_p << ","
+                << ktau << "," << a << "," << bl << "," << x0c << ","
+                << K << "," << IC << "," << avg << "," << y[max1-1][max2-1]  << "\n";
+        }
+    }
+
+ 
+     // Cleanup
+     gsl_odeiv_evolve_free(evolve);
+     gsl_odeiv_control_free(control);
+     gsl_odeiv_step_free(step);
+ 
+     return 0;
+ }
+ 
