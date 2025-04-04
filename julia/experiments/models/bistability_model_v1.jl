f6f6a8a3f2980b5ce0335a94dc02e5d5826c74a7
Jonathan St-Onge <jonathanstonge7@gmail.com>
2025-04-03 20:39:46 -0400

    bistability v1

--- julia/experiments/models/bistability_model_v1.jl
+++ julia/experiments/models/bistability_model_v1.jl
@@ -3,27 +3,20 @@ C(x, kc, x0c, bl, r) = (1 - r*bl)*(1. / (1. + exp(kc * (x - x0c)))) + bl    # co
 Π(x, a, bl, r) = (1. - r*bl)*exp(-a*x) + bl                         # group pressure to code
 
 # combine all that into a sigmoid
-tau_rescaling = 1
-τ(x, ktau, a, kc, x0c, bl, r) = tau_rescaling*(1. / (1. + 5*exp(-ktau * (B(x, a, bl, r) - C(x, kc, x0c, bl, r) + Π(x, a, bl, r)))) )
+tau_rescaling = 10
+τ(x, ktau, a, kc, x0c, bl, r) = tau_rescaling*(1. / (1. + exp(-ktau * (B(x, a, bl, r) - C(x, kc, x0c, bl, r) + Π(x, a, bl, r)))) )
 
 function J!(du, u, p, t)
-
-
     α, νn, νp, K, ktau, a, kc, x0c, bl, r = p
-    
     N, P = size(u)         
-    
     # Reset du to zero at each call
     @. du = 0.0
-
     for i=1:N, j=1:P
-            
         # Occupant numbers:
         n1 = i - 1 
         n2 = j - 1
         
         gsize = n1+n2+1 # 0..(N+P-2), e.g. N=15, P=15 -> 0..28. 
-
         du[i, j] -= νp * n2 * u[i, j]                                         # programmer death (outflow)
         du[i, j] -= νn * n1 * u[i, j]
         n1 < N-1 && (du[i, j] -= α * gsize * (1 - gsize/K) * u[i, j] )                 # non-programmer birth (outflow)
@@ -39,7 +32,7 @@ function J!(du, u, p, t)
                                         * u[i,j]
                                         ) 
             
-            (n1 < N-1 && n2 > 0 && n2 < P) && ( # Predation input  
+            (n1 < N-1 && n2 > 0 && n2 < P-1) && ( # Predation input  
                 du[i,j] += τ((n2-1)/gsize, ktau, a, kc, x0c, bl, r) 
                                 * (1. - C((n2-1)/gsize, kc, x0c, bl, r)) 
                                 * (n1+1) 
@@ -54,7 +47,10 @@ function J!(du, u, p, t)
             ) 
         end
 
-        n1 < N-1 && ( du[i,j] += τ(n2/(gsize+1), ktau, a, kc, x0c, bl, r) * C(n2/(gsize+1),  kc, x0c, bl, r) * (n1+1) * u[i+1,j] )  # Predation output
+        n1 < N-1 && ( du[i,j] += τ(n2/(gsize+1), ktau, a, kc, x0c, bl, r) 
+                                    * C(n2/(gsize+1),  kc, x0c, bl, r) 
+                                    * (n1+1) 
+                                    * u[i+1,j] )  # Predation output
    
     end
 end
